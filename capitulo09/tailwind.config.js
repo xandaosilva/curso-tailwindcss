a68e46c87634aeb6cff1e81da50e7b07e83c563a
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-02-14 18:35:31 +0044

    Tipografia

---
+++ capitulo09/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "arial": "Arial"
+      },
+      fontSize: {
+        "12xl": "8rem"
+      }
+    },
+  },
+  plugins: [],
+}
